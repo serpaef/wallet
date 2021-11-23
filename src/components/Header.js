573c1fd3b6564f88b03abc87f09c48fa28c1f26e
Fernando Serpa <serpaef@outlook.com>
2021-11-23 10:47:16 -0300

    impl: styled header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import './Header.css'
 
-function Header({email}) {
+function Header({ email }) {
   return (
     <header className="header">
-      <AccountBalanceWalletIcon sx={{fontSize: 50, color: 'white'}} />
-      <span>{ email }</span>
-      
+      <div className="user-info">
+        <AccountBalanceWalletIcon sx={ { fontSize: 50, color: '#25ABF1' } } />
+        <span>{ email }</span>
+      </div>
+      <div className="total-expenses">
+        <AttachMoneyIcon sx={ { fontSize: '30px' } } />
+        <span>BRL 12.00</span>
+      </div>
     </header>
   )
 }
@@ -18,6 +24,6 @@ Header.propTypes = {
   email: PropTypes.string,
 };
 
-const mapStateToProps = (state) => ({email: state.user.email});
+const mapStateToProps = (state) => ({ email: state.user.email });
 
 export default connect(mapStateToProps, null)(Header);
